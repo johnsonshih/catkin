b3b20ea53ca3ee9a3266f0969f5eaaa2499da8fc
Johnson Shih <jshih@microsoft.com>
2018-08-20 10:47:19 -0700

    PATCH: don't use relative path when testing init_workspace

--- python/catkin/init_workspace.py
+++ python/catkin/init_workspace.py
@@ -88,6 +88,7 @@ def init_workspace(workspace_dir):
     src = os.path.join(workspace_dir, 'catkin', 'cmake', 'toplevel.cmake')
     if os.path.isfile(src):
         src_file_path = os.path.relpath(src, workspace_dir)
+        src_file_path = src
     else:
         checked.append(src)
 
